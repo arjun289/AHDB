602d0397c67b08b4641a223dcfc667045ea68038
Gazal Kalyan <gazal@bt.iitr.ac.in>
2020-04-25 14:38:48 +0530

    changing filename

--- run_indexing.sh
+++ run_indexing.sh
@@ -1,1 +1,1 @@
-curl -H "Content-Type: application/x-ndjson" -XPOST "http://localhost:9200/peptides/_bulk" --data-binary @peptides.json
+curl -H "Content-Type: application/x-ndjson" -XPOST "http://localhost:9200/peptides/_bulk" --data-binary @alles_pep.json
